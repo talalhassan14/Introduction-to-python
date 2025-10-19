91321cc48c4bc5074cdc0fb9b3bbd631a668c3a2
talalhassan14 <talalhassan14@outlook.com>
2025-10-19 16:37:57 -0400

    intoduction to arrays

---
+++ Practise.py
@@ -0,0 +1,18 @@
+Cars=["Toyota", "Mercedize", "Honda"]
+# print(Cars[0])
+# print(Cars[1])
+# print(Cars[2])
+
+Cars[2]="Ford"
+# print(Cars)
+
+x=len(Cars)
+# print(x)
+# for x in Cars:
+#     print(x)
+
+Cars.append("Honda") # to add into an array
+print(Cars)
+Cars.pop(1)        # to delete something in an array
+print(Cars)
+print(Cars)
