2be970f60df8ed1f3557a02800a4f210ba3dbb88
talalhassan14 <talalhassan14@outlook.com>
2025-10-21 20:56:47 -0400

    2d arrays practise

--- Practise.py
+++ Practise.py
@@ -12,7 +12,26 @@ x=len(Cars)
 #     print(x)
 
 Cars.append("Honda") # to add into an array
-print(Cars)
+# print(Cars)
 Cars.pop(1)        # to delete something in an array
-print(Cars)
-print(Cars)
+# print(Cars)
+# print(Cars[1:]) # array slicing
+
+# 2d arrays by nested listed method 
+# cityTemperature=[["New York",32,42,55,60], ["Las Vegas",42,55,62,65], ["Indiana",92,82,72,47]]
+# print(cityTemperature[0][0])
+
+# 2D arrays by list comprehension method 
+rows,cols=3,4
+# array=[[0 for c in range(cols) ] for r in range(rows)]
+# print(array)
+# print(array[1][2])
+# array[1][2]=5 # assigning specifica values to the matrix 
+# print(array)
+
+# Loops through arrays
+array=[[3,4,5,6],[7,8,9,10],[11,12,13,14]]
+
+for r in range(rows):
+    for c in range(cols):
+        print(f"Row{r}, Col(c)=array[r][c])
